ea8b02c17e31a6edb119066c7ee15a92da7d71ea
Ahmadou LO <ahmadou19@gmail.com>
2020-06-05 12:03:59 +0200

    TEST DRIVEN DEVELOPMENT
    * Etape 3 : Test de l'addition et Division
    * On crée le code à tester à partir du test
    * Initialisation et libération des ressources par BEfore et After
    * Test de l'addition
    * TEst de la division avec levée d'Exception

--- src/test/java/demotdd/CalculatriceServiceTest.java
+++ src/test/java/demotdd/CalculatriceServiceTest.java
@@ -1,19 +1,46 @@
 package demotdd;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatriceServiceTest {
 	
-	@Test
-	public void TestAddition() {
-		fail(" Not yet implemented");
+	private CalculatriceService calculatriceService;
+	
+	@Before 
+	public void SetUp() {
+		calculatriceService = new CalculatriceService();
+		System.out.println("On initialise les ressources");
 	}
 	
 	@Test
-	public void TestDivisonZero() {
+	public void TestAddition() {
+		System.out.println("Test Addition");
+		int resultat = calculatriceService.Addition(3,4);
+		int expected = 7;
+		assertTrue(" Resultat not ok ",resultat==expected);
+		
+	}
+	
+	@Test(expected = ArithmeticException.class)
+	public void TestDivisonZeroWithException() {
+		System.out.println("Test Division per zero");
+		int resultat = calculatriceService.DivisionZero(3);
+		int expected = 0;
+		assertTrue(" Doit donner comme résultat 0",resultat ==expected);
+		
+		
 		fail(" Not yet implemented");
+	}
+	
+	@After 
+	public void TearDown() {
+		calculatriceService = null;
+		System.out.println("On libère les ressources");
 	}
 
 }
