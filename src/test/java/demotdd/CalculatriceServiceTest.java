246a1f164a6ab0131764f1b2d07ff958d5e84543
Lo Ahmadou <ahmadou.lo@orange.com>
2020-06-05 10:37:55 +0200

    TEST DRIVEN DEVELOPMENT
    * Etape 1 : Ecriture des scénarios
    * Etape 2 : Test fail

---
+++ src/test/java/demotdd/CalculatriceServiceTest.java
@@ -0,0 +1,19 @@
+package demotdd;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class CalculatriceServiceTest {
+	
+	@Test
+	public void TestAddition() {
+		fail(" Not yet implemented");
+	}
+	
+	@Test
+	public void TestDivisonZero() {
+		fail(" Not yet implemented");
+	}
+
+}
